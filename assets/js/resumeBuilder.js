0031a08d6ac614238ac06ede2d1142feff3c9dff
Alexander C Truong <actruong3@wichita.edu>
2015-12-20 20:21:51 -0600

    Add resume information to resumeBuilder.js.

---
+++ assets/js/resumeBuilder.js
@@ -0,0 +1,87 @@
+/**
+ * Resume info.
+ */
+
+var bio = {
+    name: "Alexander Truong",
+    role: "Application Developer",
+    contacts: {
+        mobile: "316-518-0939",
+        email: "alexctruong@gmail.com",
+        github: "https://github.com/truongski",
+        twitter: "https://twitter.com/alexctruong",
+        location: "Wichita, KS",
+    },
+    welcomeMessage: "Hi",
+    skills: [
+        "ASP.NET MVC",
+        "Django"
+    ],
+    biopic: "",
+    display: function () {
+    },
+};
+
+var education = {
+    schools: [
+        {
+            name: "Wichita State University",
+            location: "Wichita, KS",
+            degree: "Bachelor of Engineering",
+            majors: [
+                "Computer Science",
+                "Computer Engineering",
+            ],
+            dates: 2015,
+            url: "http://www.wichita.edu",
+        },
+    ],
+    onlineCourses: [
+        {
+            title: "Front End Development",
+            school: "Udacity",
+            date: 2015,
+            url: "https://www.udacity.com",
+        },
+    ],
+    display: function () {
+
+    },
+};
+
+var work = {
+    jobs: [
+        {
+            employer: "National Institute for Aviation Research",
+            title: "Senior Student Assistant",
+            location: "Wichita, KS",
+            dates: "2013-2015",
+            description: "A student assistant at NIAR typically works on projects for various companies \
+                such as Textron and Spirit and learns skills relevant to the industry. \
+                My particular position is as a programmer at the Virtual Reality lab at NIAR \
+                to design and develop Virtual Reality simulations, web applications, \
+                and internal tools using .NET, Unity, 3DVIA Studio Pro, and Unreal Engine 4. ",
+        },
+    ],
+    display: function () {
+
+    },
+};
+
+var projects = {
+    projects: [
+        {
+            title: "Shuttle Tracker",
+            dates: "2014",
+            description: "The Shuttle Tracker was developed for Wichita State University after \
+                shuttles were introduced to transport people between off-campus parking and campus. \
+                Its purpose is to display where the shuttles are on the map in real-time and predict \
+                the estimated time to a shuttle stop. ",
+            images: [
+            ],
+        },
+    ],
+    display: function () {
+
+    },
+};
